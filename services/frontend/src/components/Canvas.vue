c1a49eb2a04b07ede179cdc5c1a7b78898574a75
Christian Raue <captain_2@gmx.de>
2023-12-04 09:12:10 +0100

    initialized p5

---
+++ services/frontend/src/components/Canvas.vue
@@ -0,0 +1,37 @@
+<style>
+
+</style>
+
+<template>
+    <div id="canvas"></div>
+</template>
+
+<script>
+import P5 from 'p5';
+
+export default {
+    name: 'Canvas',
+    data() {
+        return {
+            p5canvas: null
+        }
+    },
+    methods: {
+        setup(p5) {
+            p5.createCanvas(400, 400);
+        },
+        draw(p5) {
+            p5.background(0);
+            p5.fill(255);
+            p5.rect(100, 100, 200, 200);
+        },
+    },
+    mounted() {
+        const p5Script = p5 => {
+            p5.setup = () => { this.setup(p5) };
+            p5.draw = () => { this.draw(p5) };
+        }
+        this.p5canvas = new P5(p5Script, 'canvas');
+    }
+}
+</script>
--- services/frontend/src/components/MainPage.vue
+++ services/frontend/src/components/MainPage.vue
@@ -4,18 +4,18 @@
 
 <template>
   Hello, World!
+  <Canvas />
 </template>
 
 <script>
+import Canvas from '@/components/Canvas.vue';
+
 export default {
-  name: "MainPage",
-  data() {
-    return {
-
-    };
-  },
-  methods: {
-
-  }
+    name: "MainPage",
+    data() {
+        return {};
+    },
+    methods: {},
+    components: { Canvas }
 }
 </script>
