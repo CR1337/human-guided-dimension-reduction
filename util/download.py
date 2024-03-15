0deed01dfeed32d5d47074e09f09e49afa31b05c
Christian Raue <captain_2@gmx.de>
2024-03-15 16:23:52 +0100

    refactoring

--- util/download.py
+++ util/download.py
@@ -1,23 +1,15 @@
 from datasets import load_dataset
 
 
-def download_dataset(dataset_name):
-    return load_dataset(
-        dataset_name, split=["train"], cache_dir="./volumes/data/cache"
-    )
-
-
-def save_dataset(dataset, dataset_name):
-    # Save dataset as a csv file
-    dataset.to_csv(dataset_name + ".csv")
-
-
-def main():
-    dataset_name = "imdb"
-    dataset = download_dataset(dataset_name)[0]
-    print(len(dataset))
-    save_dataset(dataset, f"./volumes/data/{dataset_name}")
+def download(csv_filename: str, name: str):
+    dataset = load_dataset(
+        name, split=['train'], cache_dir="./volumes/data/cache"
+    )[0]
+    dataset.to_csv(csv_filename)
+    return dataset
 
 
 if __name__ == "__main__":
-    main()
+    # Change args before running:
+    dataset = download("./volumes/data/imdb.csv", "imdb")
+    print(len(dataset))
